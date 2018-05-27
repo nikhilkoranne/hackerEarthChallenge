462a4df3c04ce60f9e9a861b378884fc6b5bc1c3
nikhilkoranne <koranne.nikhil@gmail.com>
2018-05-27 07:31:53 +0530

    Added Minimum Requirement with some Plus Points and some Extra Work

---
+++ app/src/app.js
@@ -0,0 +1,71 @@
+angular.module('beerOrderingApp', ['ui.router', 'checklist-model'])
+  .config([
+    '$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/menu');
+      $stateProvider
+        .state('contact', {
+          url: '/contact',
+          cache: false,
+          templateUrl: 'app/src/contactview.html',
+          controller: 'ContactController as vm'
+        })
+        .state('about', {
+          url: '/about',
+          cache: false,
+          templateUrl: 'app/src/aboutview.html',
+          controller: 'AboutController as vm'
+        })
+        .state('menu', {
+          url: '/menu',
+          cache: false,
+          templateUrl: 'app/src/menuview.html',
+          controller: 'MenuController as vm'
+        });
+    }])
+  .controller('ContactController', ['$scope', function ($scope) {
+    var vm = this;
+  }])
+  .controller('AboutController', ['$scope', function ($scope) {
+    var vm = this;
+  }])
+  .controller('MenuController', ['BeersService', '$timeout', function (BeersService, $timeout) {
+    var vm = this;
+    vm.beers = [];
+    vm.styles = [];
+    vm.selectedStyles = [];
+    vm.filteredBeers = [];
+    vm.listLoaded = false;
+
+    vm.styleSelected = function (style) {
+      $timeout(function () {
+        vm.filteredBeers = _.filter(vm.beers, function (beer) {
+          return vm.selectedStyles.includes(beer.style);
+        });
+      });
+    };
+
+    BeersService.getBeersMenu().then(function (response) {
+      vm.beers = response;
+      vm.styles = _.flatMap(_.uniqBy(vm.beers, 'style'), 'style');
+      vm.listLoaded = true;
+    });
+  }])
+  .service('BeersService', function ($http) {
+    return {
+      getBeersMenu: function () {
+        return $http({
+          method: 'GET',
+          url: 'http://starlord.hackerearth.com/beercraft'
+        })
+          .then(function (response) {
+            return response.data;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+    };
+  });
+
+angular.bootstrap(document, ['beerOrderingApp']);
